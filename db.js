f65c36e8965c6a176d54ece3ff5fea92f68dfec7
Yasha Prikhodko <yasha.prikhodko@gmail.com>
2014-11-28 10:28:04 -0500

    Added the rest of the tables.

--- db.js
+++ db.js
@@ -17,8 +17,12 @@ exports.createTables = function() {
     if (!exists) {
       return knex.schema.createTable('weapon', function(t) {
         t.increments('id').primary();
-        t.string('model', 100);
-        t.float('damage');
+        t.string('Name', 512);
+        t.string('Rate_of_fire', 512);
+        t.string('Muzzle_velocity', 512);
+        t.string('Max_dist', 512);
+        t.string('Bullet_drop', 512);
+        t.string('Img_file_loc', 512);
       });
     }
   })
@@ -36,6 +40,76 @@ exports.createTables = function() {
         });
       }
     });
+  })
+  .then(function() {
+    knex.schema.hasTable('damage').then(function(exists) {
+      if (!exists) {
+        return knex.schema.createTable('damage', function(t) {
+          t.integer('id').references('id').inTable('weapon')
+          t.string('Max_damage', 512)
+          t.string('Min_damage', 512)
+          t.string('Drop_off_start', 512)
+          t.string('Drop_off_end', 512)
+          t.string('Img_file_loc', 512)
+        });
+      }
+    })
+  })
+  .then(function() {
+    knex.schema.hasTable('reload').then(function(exists) {
+      if (!exists) {
+        return knex.schema.createTable('reload', function(t) {
+          t.integer('id').references('id').inTable('weapon')
+          t.string('Time_left', 512)
+          t.string('Time_empty', 512)
+          t.string('Time_threshold', 512)
+          t.string('Mag_size', 512)
+        });
+      }
+    })
+  })
+  .then(function() {
+    knex.schema.hasTable('recoil').then(function(exists) {
+      if (!exists) {
+        return knex.schema.createTable('recoil', function(t) {
+          t.integer('id').references('id').inTable('weapon')
+          t.string('up', 512)
+          t.string('left', 512)
+          t.string('right', 512)
+          t.string('dec', 512)
+          t.string('First_shot_mult', 512)
+        });
+      }
+    })
+  })
+  .then(function() {
+    knex.schema.hasTable('spread').then(function(exists) {
+      if (!exists) {
+        return knex.schema.createTable('spread', function(t) {
+          t.integer('id').references('id').inTable('weapon')
+          t.string('Ads_nmm', 512)
+          t.string('Ads_mm', 512)
+          t.string('Hip_s_nmm', 512)
+          t.string('Hip_c_nmm', 512)
+          t.string('Hip_p_nmm', 512)
+          t.string('Hip_s_mm', 512)
+          t.string('Hip_c_mm', 512)
+          t.string('Hip_p_mm', 512)
+          t.string('Inc', 512)
+          t.string('Dec', 512)
+        });
+      }
+    })
+  })
+  .then(function() {
+    knex.schema.hasTable('supply').then(function(exists) {
+      if (!exists) {
+        return knex.schema.createTable('supply', function(t) {
+          t.integer('id').references('id').inTable('weapon')
+          t.integer('count')
+        });
+      }
+    })
   })
 }
 
@@ -63,7 +137,6 @@ exports.createWeapons = function(weapons) {
     return Promise.resolve(msg)
   })
 }
-
 
 exports.getUsers = function() {
   return new User().fetchAll()
@@ -94,4 +167,12 @@ var createUser = exports.createUser = function(userData) {
     console.log(error, error.stack)
     return Promise.reject(error)
   })
+}
+
+var getDemoQueries = exports.getDemoQueries = function() {
+  // bookshelf.knex('weapon')
+  //   .join('Comp', 'Comp.cId', '=', 'Inv.cId')
+  //   .where('Inv.id', 2)
+  //   .select()
+  //   .then(...)
 }
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'BF4 Stats', user: req.user || null });
 });
 
+router.get('/queries', function(req, res) {
+  res.render('index', { title: 'Queries - BF4 Stats', queries: [] });
+});
+
+
 module.exports = router;
