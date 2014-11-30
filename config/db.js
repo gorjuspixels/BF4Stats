357df9cc42f38c2752f6f9b5d55dddcd9c75385d
Yasha Prikhodko <yasha.prikhodko@gmail.com>
2014-11-30 13:57:02 -0500

    Accidently commited db config file.

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 module.exports = {
   client: 'pg',
   connection: {
-    host: 'localhost',
-    user: 'bf4user',
-    password: 'not_so_secure_pass',
-    database: 'bf4',
-    charset: 'utf8'
+    host: 'ec2-54-197-240-180.compute-1.amazonaws.com',
+    user: 'amwlrhrrvmhkwa',
+    password: '6d1HBbDppijvEwsamvw_TepJze',
+    database: 'd6h6b9ppi2uopo',
+    charset: 'utf8',
+    port: 5432
   }
-}
+}z
