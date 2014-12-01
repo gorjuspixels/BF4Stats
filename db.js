c380673152842d95e46aed48eab75745064539bd
Yasha Prikhodko <yasha.prikhodko@gmail.com>
2014-12-01 09:37:19 -0500

    Fixed a bunch of issues with queries.

--- db.js
+++ db.js
@@ -197,21 +197,15 @@ exports.query3 = function() {
     .join('weapon', 'd1.id', 'weapon.id')
     .where(45, '<', knex.raw('( SELECT "Max_damage" FROM damage WHERE d1.id = id )'))
     .then(function(weapons) {
-      console.log(weapons)
       return Promise.resolve(weapons)
     })
 }
 
-
-
-
-export.query4= function(){
-   return knex.from('weapon as w1, weapon as w2')
-   knex('weapon as w1')
-    .fullJoin('weapon', 'weapon.Max_distance', 'w1.Max_distance')
-     .where('weapon.id', '!=', 'w1.id')
-    .select(['*'])
-    .orderBy('Name')
+exports.query4= function(){
+   return knex('weapon as w1')
+    .fullOuterJoin('weapon', 'weapon.Max_dist', 'w1.Max_dist')
+    .where(knex.raw('weapon.id != w1.id'))
+    .orderBy('w1.Name')
     .then(function(weapons) {
       return Promise.resolve(weapons)
     })
@@ -220,50 +214,51 @@ export.query4= function(){
 exports.query5 = function(){
   return knex('weapon as w1')
   .join('supply', 'w1.id', 'supply.id')
-  .where(1, '>', knex.raw('(SELECT "id" FROM SUPPLY WHERE count > 0)'))
+  .where(1, '=', knex.raw('(SELECT count FROM supply WHERE count > 0 AND id = w1.id )'))
   .then(function(weapons){
     return Promise.resolve(weapons)
   })
 }
 
-export.query6= function(){
-   return knex.from('weapon')
-    .Join('supply', 'weapon.id', 'supply.id')
-    .where('Max_damage', '>', knex.raw(' ALL( SELECT "Max_damage" FROM weapon)'))
-    .and (count, '>', knex.raw('ALL(SELECT "count" FROM supply)'))
-    .select(['*'])
+exports.query6= function(){
+   return knex.from('weapon as w1')
+    .join('supply', 'w1.id', 'supply.id')
+    .join('damage', 'w1.id', 'damage.id')
+    .where('damage.Max_damage', '>=', knex.raw('ALL(SELECT "Max_damage" FROM damage)'))
+    // .andWhere('count', '=', knex.raw('ALL(SELECT "count" FROM supply)'))
     .orderBy('count', 'ASC')
     .then(function(weapons) {
       return Promise.resolve(weapons)
     })
 }
 
-export.query7= function(){
+exports.query7= function(){
    return knex.from('weapon')
-    .Join('supply', 'weapon.id', 'supply.id')
-    .where('count' '>' '0')
-    .select(['Name', 'Id', 'Count'])
-    .orderBy('count' 'ASC')
+    .join('supply', 'weapon.id', 'supply.id')
+    .where('count', '>', '0')
+    .select(['Name', 'weapon.id', 'count'])
+    .orderBy('count', 'ASC')
     .then(function(weapons) {
       return Promise.resolve(weapons)
     })
 }
 
-export.query8= function(){
+exports.query8= function(){
    return knex.from('weapon')
-    .Join('spread', 'weapon.id', 'spread.id')
-    .where('Max_damage', '>', knex.raw(' ALL( SELECT "Max_damage" FROM weapon)'))
-    .andWhere(Spread_Min, '<',  knex.raw('ALL (SELECT "Spread_Min" FROM spread)'))
-    .select(['*'])
+    .join('spread', 'weapon.id', 'spread.id')
+    .join('damage', 'weapon.id', 'damage.id')
+    .where('damage.Max_damage', '>=', knex.raw(' ALL( SELECT "Max_damage" FROM damage)'))
+    // .andWhere('spread.Ads_mm', '<=',  knex.raw('ALL (SELECT "Ads_mm" FROM spread)'))
+    .orderBy('spread.Ads_mm')
     .then(function(weapons) {
       return Promise.resolve(weapons)
     })
 }
 exports.query9 = function() {
   return knex('weapon')
+  .join('damage', 'weapon.id', 'damage.id')
   .orderBy('Max_damage')
   .then(function(weapons) {
-      console.log(weapons)
       return Promise.resolve(weapons)
     })
 }
@@ -271,10 +266,9 @@ exports.query9 = function() {
 exports.query10 = function() {
   return knex('weapon')
   .join('supply', 'weapon.id', 'supply.id')
-  .where('count', '=', '0')
-  orderBy('Name')
+  .where('count', '=', 0)
+  .orderBy('Name')
   .then(function(weapons) {
-      console.log(weapons)
       return Promise.resolve(weapons)
     })
 }
